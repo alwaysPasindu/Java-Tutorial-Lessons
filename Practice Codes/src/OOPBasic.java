e7cddbc4ef4b00b5a36d9e9ada1c33cf282d2b3e
Pasindu Dilshan <pasindu.20230294@iit.ac.lk>
2024-07-04 01:32:29 +0530

    initial commit

---
+++ Practice Codes/src/OOPBasic.java
@@ -0,0 +1,32 @@
+ class Phone {
+            String name;
+            String color;
+            int ram;
+
+
+            public void call()
+            {
+                System.out.println("Take a call from " + name);
+            }
+
+            public void browseInternet()
+            {
+                System.out.println("Browse Internet");
+            }
+
+            public static void main(String[] args) {
+                Phone phone1= new Phone();
+                phone1 .name ="Samsung";
+                phone1.color = "Black";
+                phone1.call();
+
+                Phone phone2 = new Phone();
+                phone2.name = "Apple";
+                phone2.call();
+                System.out.println(phone2.color);
+            }
+
+
+    }
+
+
